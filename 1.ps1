11f04cad36585639cd28bae4abb578790ec8b56c
narimantos <narimantos@hotmail.com>
2019-09-17 00:41:09 +0200

    ye2

--- 1.ps1
+++ 1.ps1
@@ -3,11 +3,11 @@ param (
   [string]$file
     )
 write-output $path\$file
-
+#encode to wav and get only 1e channel audio
 ffmpeg -i  $path\$file -acodec pcm_s16le -ar 16000 -ac 1 "$path\$file.wav"
 $int = 0;
+#split the audio file each 58 seconds and output it like out00 
 ffmpeg -i "$path\$file.wav" -f segment -segment_time 58 -c copy "$path\out%03d.wav"
-$operation = "operation"
 Get-ChildItem "$path" -Filter "out*.wav" |
 Foreach-Object {
   $content = Get-ItemProperty $_.FullName
@@ -17,7 +17,7 @@ Foreach-Object {
     Start-Sleep -s 60
     $transcript = gcloud ml speech operations describe $_.name
     Write-Output( $transcript | ConvertFrom-Json | ForEach-Object {
-      $transcript
+      $transcript >> "$path\out%03d.txt"
     })
     $_.name})
   $int++
